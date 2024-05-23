db32906208b46fa4b6ae2e85c10e047656210659
Pradeepal Ariyasena <dgpradeepal@gmail.com>
2024-05-23 15:25:53 +0530

    adds simple route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './modules/plants/components/list/list.component';
+import { DetailComponent } from './modules/plants/components/detail/detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'plant', component: ListComponent },
+  { path: 'plant2', component: DetailComponent },
+  { path: '', redirectTo: '/plant', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', redirectTo: '/plant', pathMatch: 'full' }, // Wildcard route for a 404 page
+];
