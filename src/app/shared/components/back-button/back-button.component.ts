4417dc3ff5cdcd2387cd3fd1bcc0ffc371f96e3c
Pradeepal Ariyasena <dgpradeepal@gmail.com>
2024-05-23 21:39:18 +0530

    Adds shared components

---
+++ src/app/shared/components/back-button/back-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-back-button',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './back-button.component.html',
+  styleUrl: './back-button.component.scss',
+})
+export class BackButtonComponent {
+  @Input() title: string = 'Back';
+}
--- src/app/shared/types/PlantsState.interface.ts
+++ src/app/shared/types/PlantsState.interface.ts
@@ -4,4 +4,5 @@ export interface PlantsStateInterface {
   isLoading: boolean;
   error: string | null;
   data: GetPlantsResponseInterface | null;
+  isNavigated: boolean;
 }
