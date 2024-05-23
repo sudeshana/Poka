2ba6160a3d5b997cd951b8eedfa17082d568a63c
Pradeepal Ariyasena <dgpradeepal@gmail.com>
2024-05-23 21:42:41 +0530

    Adds CardComponent

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  constructor(private readonly router: Router) {}
+
+  gotoDetails() {
+    this.router.navigate(['/details']);
+  }
+}
