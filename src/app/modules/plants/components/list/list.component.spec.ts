4aa13922102809b5ad489558cba2469ce5fe8a21
Pradeepal Ariyasena <dgpradeepal@gmail.com>
2024-05-24 14:36:59 +0530

    Tests list component

--- src/app/modules/plants/components/list/list.component.spec.ts
+++ src/app/modules/plants/components/list/list.component.spec.ts
@@ -1,17 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../../../shared/components/header/header.component';
+import { CardComponent } from '../../../../shared/components/card/card.component';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { By } from '@angular/platform-browser';
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  const initialState = {
+    plants: {
+      isLoading: false,
+      error: null,
+      data: {
+        count: 50,
+        next: '?offset=10',
+        previous: null,
+        results: [
+          {
+            address: '119 Webb View Apt. 563',
+            country: 'VC',
+            division: 'Aerospace',
+            id: 1,
+            name: 'Wallace-Patton',
+          },
+        ],
+      },
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        ListComponent,
+        CommonModule,
+        HeaderComponent,
+        CardComponent,
+        ButtonComponent,
+        RouterTestingModule,
+      ],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +53,11 @@ describe('ListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Renders Plants List', () => {
+    const posts = fixture.debugElement.queryAll(By.css('[data-testid="card"]'));
+    expect(posts.length).toEqual(1);
+    expect(posts[0].nativeElement.textContent).toContain('Aerospace');
   });
 });
