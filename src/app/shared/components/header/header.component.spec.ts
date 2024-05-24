bf7a738f3e1883dd7ff11e4a832700fdb8d8f0ad
Pradeepal Ariyasena <dgpradeepal@gmail.com>
2024-05-24 12:57:28 +0530

    Tests Header component

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -1,23 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HeaderComponent],
+    }).compileComponents();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
-    
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('creates component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Renders default state', () => {
+    const HeaderComponent = fixture.debugElement.query(
+      By.css('[data-testid="title"]')
+    );
+
+    expect(HeaderComponent.nativeElement.textContent).toEqual(' header ');
+  });
+
+  it('Renders custom state', () => {
+    component.description = 'Testing custom description';
+    fixture.detectChanges();
+
+    const HeaderComponent = fixture.debugElement.query(
+      By.css('[data-testid="description"]')
+    );
+    expect(HeaderComponent.nativeElement.textContent).toEqual(
+      ' Testing custom description '
+    );
   });
 });
