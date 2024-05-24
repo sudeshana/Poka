34b13ae9130d6094fd7e0bfc583e3fd971d251a7
Pradeepal Ariyasena <dgpradeepal@gmail.com>
2024-05-24 10:25:03 +0530

    Tests Util services

--- src/app/core/services/persistance.service.spec.ts
+++ src/app/core/services/persistance.service.spec.ts
@@ -3,14 +3,42 @@ import { TestBed } from '@angular/core/testing';
 import { PersistanceService } from './persistance.service';
 
 describe('PersistanceService', () => {
-  let service: PersistanceService;
+  let persistanceService: PersistanceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PersistanceService);
+    TestBed.configureTestingModule({
+      providers: [PersistanceService],
+    });
+    persistanceService = TestBed.inject(PersistanceService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('creates the service', () => {
+    expect(persistanceService).toBeTruthy();
+  });
+
+  describe('set', () => {
+    it('Should save in localStorage', () => {
+      jest.spyOn(Storage.prototype, 'setItem');
+      persistanceService.set('KEY', {});
+      expect(localStorage.setItem).toHaveBeenCalled();
+    });
+  });
+  describe('get', () => {
+    it('Should get from localStorage', () => {
+      jest.spyOn(Storage.prototype, 'getItem');
+      persistanceService.get('KEY');
+      expect(localStorage.getItem).toHaveBeenCalled();
+    });
+  });
+  describe('delete', () => {
+    it('Should delete from localStorage', () => {
+      jest.spyOn(Storage.prototype, 'removeItem');
+      persistanceService.delete('KEY');
+      expect(localStorage.removeItem).toHaveBeenCalled();
+    });
   });
 });
